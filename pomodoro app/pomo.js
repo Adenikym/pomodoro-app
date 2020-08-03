f63d92529e4a5fbdd92f901830c17a412a397b20
Adenikym <mm5adeniky@gmail.com>
2020-08-03 13:18:17 +0100

    first commit

---
+++ pomodoro app/pomo.js
@@ -0,0 +1,80 @@
+const five = document.getElementById("five");
+const half = document.getElementById("half");
+const set = document.getElementById("set");
+const display= document.getElementById("seconds");
+const middle = document.getElementById("mains");
+const first = document.getElementById("hours");
+
+
+let count = document.getElementById("count");
+const start = document.getElementById("start");
+
+function timer (seconds) {
+    const now= Date.now();
+    const then= now + (seconds*1000)
+console.log([now,then])
+
+   
+const countdown=setInterval(function () {
+    const secondsLeft=  then-Date.now();
+    showTime(Math.round(secondsLeft/1000));
+    console.log(Math.round(secondsLeft/1000));
+    if (secondsLeft<0 ){
+
+        clearInterval(countdown);
+        return;
+    }
+   
+    
+
+    
+   
+    
+
+
+
+   
+    
+}
+ ,-1)
+
+    
+
+
+    
+}
+
+function showTime(seconds){
+    
+    const lowest= Math.floor(seconds/60);
+    const minutes=Math.floor( seconds%60);
+    const hours= seconds/3600;
+    display.innerHTML=minutes;
+    middle.innerHTML=lowest;
+    
+}
+
+start.addEventListener("click",function() { return timer(1800);
+}
+    
+)
+
+
+
+
+
+
+
+
+
+
+
+
+five.addEventListener("click",function () {
+    return timer(300);
+
+})
+
+
+
+
